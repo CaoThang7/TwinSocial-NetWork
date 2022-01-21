4d3bc1cfcded20b2ee5fc48d96925919840b9afa
CaoThang <thangly2k1@gmail.com>
2022-01-22 05:15:29 +0700

    Custom Screen Register (add register gender and birthday for user)

--- lib/Components/Register/body.dart
+++ lib/Components/Register/body.dart
@@ -4,7 +4,9 @@ import 'package:flutter_svg/svg.dart';
 import 'package:twin_social_network/Components/Register/input_field.dart';
 import 'package:twin_social_network/Components/Register/styles.dart';
 import 'package:twin_social_network/Components/Register/valid.dart';
+import 'package:twin_social_network/Models/Gender/GenderModel.dart';
 import 'package:twin_social_network/NetWork/NetworkHandler.dart';
+import 'package:intl/intl.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -19,6 +21,8 @@ class _BodyState extends State<Body> {
   final _confirmController = TextEditingController();
   bool visPassword = true;
   bool visConfirmPassword = true;
+  DateTime _selectedDate = DateTime.now();
+  String _gender = "Nam";
   final _globalkey = GlobalKey<FormState>();
   NetworkHandler networkHandler = NetworkHandler();
   // Hàm onclick đăng ký check validate
@@ -30,6 +34,8 @@ class _BodyState extends State<Body> {
         "username": _usernameController.text,
         "email": _emailController.text,
         "password": _passwordController.text,
+        "gender": _gender,
+        "birthday": DateFormat.yMd().format(_selectedDate),
       };
       print(data);
 
@@ -42,6 +48,48 @@ class _BodyState extends State<Body> {
         print("That bai");
       }
       ;
+    }
+  }
+  //Show Calendar
+  Future<void> _getDateFromUser(BuildContext context) async {
+      DateTime? picked = await showDatePicker(
+        context: context,
+        builder: (BuildContext context, Widget ?child) {
+        //Custom Theme Calendar
+        return Theme(
+          data: ThemeData(
+            primarySwatch: Colors.grey,
+            splashColor: Colors.black,
+            textTheme: TextTheme(
+            subtitle1: TextStyle(color: Colors.black),
+            button: TextStyle(color: Colors.black),
+          ),
+          colorScheme: ColorScheme.light(
+            primary: AppColors.baseOrangeColor,
+            primaryVariant: Colors.black,
+            secondaryVariant: Colors.black,
+            onSecondary: Colors.black,
+            onPrimary: Colors.white,
+            surface: Colors.black,
+            onSurface: Colors.black,
+            secondary: Colors.black),
+            dialogBackgroundColor: Colors.white,
+        ),
+        child: child ??Text(""),
+      );
+    }
+    initialDate: DateTime.now(),
+    firstDate: DateTime(1960, 8),
+    lastDate: DateTime.now()
+    );
+    //Nếu giá trị khi người dùng chọn trong calendar khác rỗng
+    //Biến _selectedDate sẽ = giá trị người dùng chọn
+    if(picked != null){
+      setState(() {
+        _selectedDate = picked;
+      });
+    }else{
+      print("Some thing went wrong");
     }
   }
 
@@ -66,8 +114,9 @@ class _BodyState extends State<Body> {
             key: _globalkey,
             child: Column(
               children: [
+                // Input Họ và tên
                 InputRegister(
-                  hint: "fullname",
+                  hint: "Họ và tên",
                   controller: _fullnameController,
                   valid_input: validFullName,
                   suffixIcon: _fullnameController.text.isEmpty
@@ -82,8 +131,9 @@ class _BodyState extends State<Body> {
                         ),
                   visBool: false,
                 ),
+                // Input Tên tài khoản
                 InputRegister(
-                  hint: "username",
+                  hint: "Tên tài khoản",
                   controller: _usernameController,
                   valid_input: validUserName,
                   suffixIcon: _usernameController.text.isEmpty
@@ -98,6 +148,7 @@ class _BodyState extends State<Body> {
                         ),
                   visBool: false,
                 ),
+                // Input Email
                 InputRegister(
                   hint: "email",
                   controller: _emailController,
@@ -114,8 +165,9 @@ class _BodyState extends State<Body> {
                         ),
                   visBool: false,
                 ),
+                // Input Password
                 InputRegister(
-                  hint: "password",
+                  hint: "Mật khẩu",
                   controller: _passwordController,
                   valid_input: validPassword,
                   suffixIcon: IconButton(
@@ -130,8 +182,9 @@ class _BodyState extends State<Body> {
                   ),
                   visBool: visPassword,
                 ),
+                // Input Confirm Password
                 InputRegister(
-                  hint: "confirm",
+                  hint: "Xác nhận mật khẩu",
                   controller: _confirmController,
                   valid_input: validConfirmPassword,
                   suffixIcon: IconButton(
@@ -145,6 +198,54 @@ class _BodyState extends State<Body> {
                     },
                   ),
                   visBool: visConfirmPassword,
+                ),
+                // Hàng chứa input ngày sinh và giới tính
+                Row(
+                  children: [
+                    // Input giới tính
+                    Expanded(
+                      child: InputRegister(
+                        hint: _gender,
+                        suffixIcon: DropdownButtonHideUnderline(
+                          child: DropdownButton(
+                            elevation: 0,
+                            iconSize: 32,
+                            // lấy dữ liệu từ GenderModel và map ra
+                            items: GenderModel().genderList.map((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value.toString(),
+                                child: Text(
+                                  value.toString(),
+                                ),
+                              );
+                            }).toList(),
+                            onChanged: (String? value) {
+                              setState(() {
+                                _gender = value!;
+                              });
+                            },
+                          ),
+                        ),
+                        visBool: false,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    // Input ngày sinh
+                    Expanded(
+                      child: InputRegister(
+                        hint: DateFormat.yMd().format(_selectedDate),
+                        suffixIcon: IconButton(
+                          icon: SvgPicture.asset("assets/icons/calendar.svg"),
+                          onPressed: () {
+                            _getDateFromUser(context);
+                          },
+                        ),
+                        visBool: false,
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(
                   height: 10,
---
+++ lib/Models/Gender/GenderModel.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class GenderModel {
+  List<String> genderList = [
+    "Nam", 
+    "Nữ",
+    "Khác"
+  ];
+}
