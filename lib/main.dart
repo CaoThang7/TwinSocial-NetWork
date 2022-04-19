dc83013048fbd10df50e19cc4a77be1dcf22337a
CaoThang <thangly2k1@gmail.com>
2022-04-19 22:14:39 +0700

    GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:twin_social_network/Screens/Home/HomeScreen.dart';
 import 'package:twin_social_network/Screens/Login/LoginScreen.dart';
 import 'package:twin_social_network/Screens/Register/RegisterScreen.dart';
 import 'package:twin_social_network/Screens/SplashScreen/WelcomeScreen.dart';
-
+import 'package:get/get.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: WelcomeScreen(),
       routes: {
