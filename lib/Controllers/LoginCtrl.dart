8c527486d610ff14d6bab74d0653771682d488af
CaoThang <thangly2k1@gmail.com>
2022-04-20 10:14:41 +0700

    delete GlobalKey in GetxController

--- lib/Controllers/LoginCtrl.dart
+++ lib/Controllers/LoginCtrl.dart
@@ -13,21 +13,20 @@ class LoginController extends GetxController {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   var loadingController = Get.put(LoadingController());
-  final globalkey = GlobalKey<FormState>();
+
   void login() async {
-    if (globalkey.currentState!.validate()) {
-      LoginModel loginModel = LoginModel(
-        email: emailController.text,
-        password: passwordController.text,
-      );
-      var response = await NetworkHandler.post(loginModelToJson(loginModel), "login");
-      var data = json.decode(response);
-      await NetworkHandler.storeToken(data["access_token"]);
-      print(data["msg"]);
-      if (data["msg"] == "Login Success!") {
-        await loadingController.loadingButton();
-        Navigation.homeScreen;
-      }
+    LoginModel loginModel = LoginModel(
+      email: emailController.text,
+      password: passwordController.text,
+    );
+    var response =
+        await NetworkHandler.post(loginModelToJson(loginModel), "login");
+    var data = json.decode(response);
+    await NetworkHandler.storeToken(data["access_token"]);
+    print(data["msg"]);
+    if (data["msg"] == "Login Success!") {
+      await loadingController.loadingButton();
+      Get.toNamed("/homeScreen");
     }
   }
 }
