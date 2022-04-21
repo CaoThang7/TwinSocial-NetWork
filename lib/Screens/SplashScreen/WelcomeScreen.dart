3a3d3968d3f0d832d129923d9e986ce3f72b79fa
CaoThang <thangly2k1@gmail.com>
2022-04-21 23:54:14 +0700

    use homeController

--- lib/Screens/SplashScreen/WelcomeScreen.dart
+++ lib/Screens/SplashScreen/WelcomeScreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:twin_social_network/Components/SplashScreen/body.dart';
+import 'package:twin_social_network/Controllers/HomeCtrl.dart';
 import 'package:twin_social_network/Service/NetWork/NetworkHandler.dart';
 import 'package:get/get.dart';
 
@@ -11,28 +11,7 @@ class WelcomeScreen extends StatefulWidget {
 
 class WelcomeScreenSate extends State<WelcomeScreen> {
   Widget page = WelcomeScreen();
-  final storage = FlutterSecureStorage();
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _checkLogin();
-  }
-
-  // Kiểm tra token từ storage khi user đăng nhập
-  // Nếu khác null 3s sau chuyển vào màn hình chính homeScreen()
-  // Nếu null vào screen loginScreen()
-  void _checkLogin() async {
-    var token = await NetworkHandler.getToken("access_token");
-    if (token != null) {
-      await Future.delayed(Duration(milliseconds: 3000), () {});
-      Get.toNamed("/homeScreen");
-    } else {
-      await Future.delayed(Duration(milliseconds: 3000), () {});
-      Get.toNamed("/loginScreen");
-    }
-  }
-
+  var homeController = Get.put(HomeController()); //checkLogin
   @override
   Widget build(BuildContext context) {
     return Body(); // body WelcomeScreen
