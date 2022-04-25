5f4c4efde05402a908e8c8801859e45894ed45c5
CaoThang <thangly2k1@gmail.com>
2022-04-25 23:51:04 +0700

    change AppUrl

--- lib/Utils/App_Url.dart
+++ lib/Utils/App_Url.dart
@@ -1,5 +1,3 @@
 class AppUrl {
   static const String baseUrl = 'http://10.0.3.2:5000/api/';
-  static const String login = baseUrl + '/ApiLogin';
-  static const String register = baseUrl + '/ApiRegister';
 }
