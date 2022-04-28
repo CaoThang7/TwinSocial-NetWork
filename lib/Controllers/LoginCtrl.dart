618f971aea76bcf81a6f6e0612c73c3ddbf0c6fc
CaoThang <thangly2k1@gmail.com>
2022-04-28 20:12:42 +0700

    void logout

--- lib/Controllers/LoginCtrl.dart
+++ lib/Controllers/LoginCtrl.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:twin_social_network/AppColors/app_colors.dart';
 import 'package:twin_social_network/Controllers/LoadingCtrl.dart';
 import 'package:twin_social_network/Models/Login/LoginModel.dart';
 import 'package:twin_social_network/Navigation/Navigation.dart';
@@ -14,6 +15,7 @@ class LoginController extends GetxController {
   TextEditingController passwordController = TextEditingController();
   var loadingController = Get.put(LoadingController());
 
+  // Login
   void login() async {
     LoginModel loginModel = LoginModel(
       email: emailController.text,
@@ -27,6 +29,37 @@ class LoginController extends GetxController {
     if (data["msg"] == "Login Success!") {
       await loadingController.loadingButton();
       Get.toNamed("/homeScreen");
+      emailController.clear(); // after login value email clear
+      passwordController.clear(); // after login value password clear
+    }
+  }
+
+  // Logout
+  void logout() async {
+    LoginModel loginModel = LoginModel(
+      email: emailController.text,
+      password: passwordController.text,
+    );
+    var response =
+        await NetworkHandler.post(loginModelToJson(loginModel), "logout");
+    var data = json.decode(response);
+    // if Logged out success: show dialog confirm or cancel
+    if (data["msg"] == "Logged out!") {
+      Get.defaultDialog(
+        title: "Bạn có chắc chắn đăng xuất?",
+        titleStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        middleText: "TwinSocial xin chào và hẹn gạp lại!",
+        textCancel: "Huỷ",
+        cancelTextColor: Colors.black,
+        textConfirm: "Đồng ý",
+        confirmTextColor: Colors.white,
+        buttonColor: AppColors.baseDarkOrangeColor,
+        onCancel: () {},
+        onConfirm: () async {
+          await NetworkHandler.removeToken(); // remove token in storage
+          Get.toNamed("/loginScreen");
+        },
+      );
     }
   }
 }
