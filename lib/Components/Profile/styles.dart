2feb28d5c7c23d21d227e8ecf2256eff3bb1e945
CaoThang <thangly2k1@gmail.com>
2022-04-28 13:32:20 +0700

    style profile screen

---
+++ lib/Components/Profile/styles.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:twin_social_network/AppColors/app_colors.dart';
+
+TextStyle get textFullname{
+  return(
+    TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.bold,
+      color: AppColors.baseDarkOrangeColor
+    )
+  );
+} 
+
+TextStyle get textEmail{
+  return(
+    TextStyle(
+      fontSize: 18,
+    )
+  );
+} 
+
+TextStyle get textNumber{
+  return(
+    TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.bold,
+    )
+  );
+} 
+
+TextStyle get textFollowers{
+  return(
+    TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+    )
+  );
+} 
+
+TextStyle get textFollow{
+  return(
+    TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+    )
+  );
+} 
+
+
