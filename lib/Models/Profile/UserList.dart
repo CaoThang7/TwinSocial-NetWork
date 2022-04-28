1f44542dcf1049efc5c86e7b2cb26062d5e8c3d3
CaoThang <thangly2k1@gmail.com>
2022-04-28 13:34:52 +0700

    user list

---
+++ lib/Models/Profile/UserList.dart
@@ -0,0 +1,9 @@
+class UserDataList {
+  static String id = "";
+  static String fullname = "";
+  static String username = "";
+  static String email = "";
+  static String password = "";
+  static String gender = "";
+  static String birthday = "";
+}
