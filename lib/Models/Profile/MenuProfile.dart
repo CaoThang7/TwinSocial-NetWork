a91c46bfa4f458d5d17c55946a29952542f6c831
CaoThang <thangly2k1@gmail.com>
2022-04-28 19:04:50 +0700

    menu profile model

---
+++ lib/Models/Profile/MenuProfile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MenuProfile {
+  final String text;
+  final IconData icon;
+  const MenuProfile({
+     required this.text,
+     required this.icon,
+  });
+}
