d71f8924cdb0142041f9e05af0c1bce1e02fd59d
CaoThang <thangly2k1@gmail.com>
2022-04-29 16:42:17 +0700

    http patch

--- lib/Service/NetWork/NetworkHandler.dart
+++ lib/Service/NetWork/NetworkHandler.dart
@@ -18,6 +18,17 @@ class NetworkHandler {
     return response.body;
   }
 
+  // http patch
+  static Future<String> patch(
+      var body, String endpoint, String? access_token) async {
+    var response = await client.patch(buildUrl(endpoint), body: body, headers: {
+      "Content-type": "application/json",
+      "authorization": "Bearer $access_token"
+    });
+    print(response.body);
+    return response.body;
+  }
+
   // http get
   static Future<dynamic> get(String endpoint, String? access_token) async {
     var response = await client.get(buildUrl(endpoint), headers: {
