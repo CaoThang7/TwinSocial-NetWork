1b9c9d1b78f0c7fe0a86eb5ba6c13a7c5e2a64dd
CaoThang <thangly2k1@gmail.com>
2022-05-09 23:30:33 +0700

    homeScreenItems (add SearchScreen)

--- lib/Utils/global_variable.dart
+++ lib/Utils/global_variable.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:twin_social_network/Screens/Profile/ProfileScreen.dart';
+import 'package:twin_social_network/Screens/Search/SearchScreen.dart';
 
 List<Widget> homeScreenItems = [
   const Text('a'),
-  const Text('b'),
+  SearchScreen(),
   const Text('c'),
   const Text('d'),
   ProfileScreen(),
